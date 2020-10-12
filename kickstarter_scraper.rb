b0237f852b3494071f1a6090e6670e691055ee69
Sierra Wallace <sskwallace@gmail.com>
2020-10-12 04:25:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 def create_project_hash
     html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  #binding.pry
 end
 create_project_hash
 
